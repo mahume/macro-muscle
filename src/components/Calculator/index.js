d0225ee23a211d87f64b850b5a7d0daeb1dd7ed5
mahume <michael.a.hume@gmail.com>
2019-10-02 22:36:30 -0600

    Setup: Grid layout for Calc

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Section } from "./styles";
+
+const Calculator = () => {
+  return (
+    <Section />
+  )
+};
+
+export default Calculator;
---
+++ src/components/Calculator/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  background-color: salmon;
+
+  grid-area: calc;
+`
