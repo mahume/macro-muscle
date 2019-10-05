723e4deba6cbd034c20cd1cfa8eaec28b52ed84f
mahume <michael.a.hume@gmail.com>
2019-10-05 17:12:20 -0600

    Add: imperialToMetric() calc

--- src/utils/equations.js
+++ src/utils/equations.js
@@ -1,1 +1,15 @@
-const bodyMassIndex = (m, kg) => kg / m**2;
+const imperialToMetric = (inches, pounds) => {
+  return inches * 0.0254
+}
+
+const bodyMassIndex = (height, weight) => weight / height**2;
+
+const basalMetabolicRate = (isMale, kg, m, age) => {
+  if (isMale) {
+    return 66.47 + (6.24 * kg) + (12.7 * m) - (6.755 * age);
+  }
+  return 655.1 + (4.35 * kg) + (4.7 * m) - (4.7 * age);
+}
+
+console.log(imperialToMetric(76.5));
+
