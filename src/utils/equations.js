eaf767b0c8c285ab8dad33f636a1105a310bd8d8
mahume <michael.a.hume@gmail.com>
2019-10-05 18:08:08 -0600

    Add: inchesToCentimeters, poundsToKilograms, BMR & BMI formulae

--- src/utils/equations.js
+++ src/utils/equations.js
@@ -1,16 +1,20 @@
-const inchesToMeters = inches => inches * 0.0254;
+const inchesToCentimeters = inches => inches * 2.54;
 
-const poundsToKilograms = pounds => pounds * 0.45359237;
+const poundsToKilograms = pounds => pounds * 0.453592;
 
-const bodyMassIndex = (meters, kilograms) => kilograms / meters**2;
+const bodyMassIndex = (centimeters, kilograms) => kilograms / (centimeters / 100)**2;
 
-const basalMetabolicRate = (isMale, kg, m, age) => {
+const basalMetabolicRate = (isMale, centimeters, kilograms, yearsOld) => {
   if (isMale) {
-    return 66.47 + (6.24 * kg) + (12.7 * m) - (6.755 * age);
+    return 66.47 + (13.75 * kilograms) + (5.003 * centimeters) - (6.755 * yearsOld);
   }
-  return 655.1 + (4.35 * kg) + (4.7 * m) - (4.7 * age);
+  return 655.1 + (9.563 * kilograms) + (1.85 * centimeters) - (4.676 * yearsOld);
 }
 
+console.log(poundsToKilograms(215));
+console.log(inchesToCentimeters(77));
+console.log(bodyMassIndex(inchesToCentimeters(77), poundsToKilograms(215)));
+console.log(basalMetabolicRate(false, inchesToCentimeters(65), poundsToKilograms(115), 31));
 
-const myBMI = bodyMassIndex(inchesToMeters(77), poundsToKilograms(215))
-console.log(`My BMI: ${myBMI}`);
+
+
